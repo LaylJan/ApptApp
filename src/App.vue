5c2b84f4b68d6259a8dcc3f8247e224262e89b19
Layljan <layljandee@gmail.com>
2024-11-28 18:24:38 +0800

    feat: added footer

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <Navbar />
   <LandingPage msg="Welcome to Your Vue.js App"/>
+  <Footer></Footer>
 </template>
 
 <script>
 
 import LandingPage from './components/LandingPage.vue'
 import Navbar from './components/Navbar.vue';
+import Footer from './components/footer.vue';
 
 export default {
   name: 'App',
   components: {
     LandingPage,
-    Navbar
+    Navbar,
+    Footer
   }
 }
 </script>
